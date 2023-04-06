49fde2def6840e9ee25f2a09adcc87a027041611
arhammusheer <arhammusheer@gmail.com>
2023-04-06 00:43:55 -0400

    Added protection on route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller";
+import { userMiddleware } from "../middlewares/user.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("/login", userController.login);
 userRoutes.post("/register", userController.register);
 
+userRoutes.use(userMiddleware);
 userRoutes.get("/", userController.isLoggedIn);
 userRoutes.get("/logout", userController.logout);
 
