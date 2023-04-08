e78497de495666dd7a890d6c2fc68aff5f26c66c
arhammusheer <arhammusheer@gmail.com>
2023-04-08 00:50:18 -0400

    cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,12 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", /\.croissant\.one$/],
+    origin: [
+      "http://localhost:3000",
+      /\.croissant\.one$/,
+      /\.filemind\.dev$/,
+      "https://filemind.dev",
+    ],
     credentials: true,
   })
 );
